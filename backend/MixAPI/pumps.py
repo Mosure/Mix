0dfceea52faf49c66e14be82695448a0c983b865
Mitchell Mosure <hctimit@gmail.com>
2018-11-30 17:02:19 -0600

    ramp logic

--- backend/MixAPI/pumps.py
+++ backend/MixAPI/pumps.py
@@ -10,7 +10,9 @@ usleep = lambda x: time.sleep(x/1000000.0)
 ENABLE_PIN = 5
 DIRECTION_PIN = 6
 PULSE_PIN = 13
-CRITICAL_DELAY = 1000  # Microseconds
+CRITICAL_DELAY = 500  # Microseconds
+MAX_SPEED = 500  # Microseconds per step
+RAMP_START = 1500
 
 def init():
     if not HARDWARE_ENABLED:
@@ -18,7 +20,7 @@ def init():
 
     GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(ENABLE_PIN, GPIO.OUT)  # Enable
+    GPIO.setup(ENABLE_PIN, GPIO.OUT, initial=GPIO.HIGH)  # Enable
     GPIO.setup(DIRECTION_PIN, GPIO.OUT)  # Direction
     GPIO.setup(PULSE_PIN, GPIO.OUT)  # Pulse
 
@@ -27,6 +29,16 @@ def pump(milliliters):
         return
 
     steps = abs(milliliters)
+
+    ramp_up = False
+    if steps > 200:
+        steps = steps - 200
+        ramp_up = True
+    
+    ramp_down = False
+    if steps > 200:
+        steps = steps - 200
+        ramp_down = True
 
     # Enable the motor
     if milliliters > 0:
@@ -38,15 +50,36 @@ def pump(milliliters):
 
     usleep(CRITICAL_DELAY)
 
+    if ramp_up:
+        _ramp_up()  # Does 200 steps
+
+    speed = RAMP_START
+    if ramp_up:
+        speed = MAX_SPEED
+
     for i in range(steps):
-        _step()
+        _step(speed)
+    
+    if ramp_down:
+        _ramp_down()  # Does 200 steps
     
     usleep(CRITICAL_DELAY)
 
     GPIO.output(ENABLE_PIN, GPIO.HIGH)
 
-def _step():
+def _step(delay):
     GPIO.output(PULSE_PIN, GPIO.HIGH)
-    usleep(CRITICAL_DELAY)
+    usleep(delay)
     GPIO.output(PULSE_PIN, GPIO.LOW)
-    usleep(CRITICAL_DELAY)
+    usleep(delay)
+
+def _ramp_eq(x, steps, ramp_start, ramp_end):
+    return ((ramp_end - ramp_start) / steps) * x + ramp_start
+
+def _ramp_up():
+    for i in range(200):
+        _step(_ramp_eq(i, 200, RAMP_START, MAX_SPEED))
+
+def _ramp_down():
+    for i in range(200):
+        _step(_ramp_eq(i, 200, MAX_SPEED, RAMP_START))
